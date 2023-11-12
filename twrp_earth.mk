7a3c84094f848d0adc092a1a76aab5d5b6ba2c61
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2023-11-12 22:32:29 +0700

    Update

---
+++ twrp_earth.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from earth device
+$(call inherit-product, device/xiaomi/earth/device.mk)
+
+PRODUCT_DEVICE := earth
+PRODUCT_NAME := twrp_earth
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 12C
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi 12C
